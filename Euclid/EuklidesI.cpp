9cedf571a94c8ed21d31401d1b042cc36ee9862a
Kubabinho <133696920+Kubabinho@users.noreply.github.com>
2023-05-23 20:22:30 +0200

    Add files via upload

---
+++ Euclid/EuklidesI.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int EuI(int n1, int b);
+
+int main() {
+    int n1, n2;
+
+    cout << "Enter numbers: "<<endl;
+    cin >> n1 >> n2;
+
+    cout << EuI(n1, n2) << endl;
+
+    return 0;
+}
+int EuI(int n1, int n2) {
+    while (n1 != n2) {
+        if (n1 > n2) {
+            n1 -= n2;
+        }
+        else {
+            n2 -= n1;
+        }
+        return n1;
+    }
+}
